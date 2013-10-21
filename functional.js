d2d70027dd185dbe77d47847e0b05536184733e4
vadimiztveri <vadimiztveri@yandex.ru>
2013-10-21 15:35:41 +0400

    production

--- functional.js
+++ functional.js
@@ -118,16 +118,73 @@ var Day = function (day, first_day) {
   }
 }
 
-
 /**
  * Запускает отображение календаря
  * @param {Object} month
  */
 Calendar.prototype.draw = function () {
-   draw_new_year();
-   draw_new_month();
-   draw_new_days();
+   this.draw_new_year();
+   this.draw_new_month();
+   this.draw_new_days();
 };
+
+/**
+ * Переписывает год, и ближайшие два года (до и после) в календаре.
+ * @example
+ * draw_new_year();
+ */
+Calendar.prototype.draw_new_year = function() {
+   document.getElementById('year-minus').innerHTML = (New_Calender.current_date[0] - 1);
+   document.getElementById('year-selected').innerHTML = New_Calender.current_date[0];
+   document.getElementById('year-plus').innerHTML = (New_Calender.current_date[0] + 1);
+}
+
+/**
+ * Выводит дату текстом в поле ввода.
+ * Не получает и не возвращает значений.
+ */
+Calendar.prototype.draw_full_date_in_area = function(){
+  document.getElementById('result').value = New_Calender.selected_date[2] + " " + New_Calender.years[New_Calender.current_date[0]].months[New_Calender.current_date[1]].name_case + " " + New_Calender.selected_date[0];
+}
+
+
+/**
+ * Переписывает месяц, и ближайшие два месяца (до и после) в календаре.
+ * @example
+ * draw_new_month();
+ */
+Calendar.prototype.draw_new_month = function() {
+  document.getElementById('month-minus').innerHTML = New_Calender.years[New_Calender.current_date[0]].months[1].NAMES[New_Calender.current_date[1]];
+  document.getElementById('month-selected').innerHTML = New_Calender.years[New_Calender.current_date[0]].months[1].NAMES[New_Calender.current_date[1] + 1];
+  document.getElementById('month-plus').innerHTML = New_Calender.years[New_Calender.current_date[0]].months[1].NAMES[New_Calender.current_date[1] + 2];
+}
+
+/**
+ * Меняет отображение всех дат в одном месяце.
+ * @example
+ * draw_new_days();
+ */
+Calendar.prototype.draw_new_days = function() {
+  var text_days = "",
+      month = New_Calender.years[New_Calender.current_date[0]].months[New_Calender.current_date[1]];
+
+  for (i = 0; i < month.first_day(); i++) {
+      text_days += "<li class='empty'>&nbsp;</li>";
+  }
+
+  for (i = 0; i < month.days_count(); i++) {
+    if (month.days[i].output) {
+      text_days += "<li class='calendar-day weekend";
+    } else {
+        text_days += "<li class='calendar-day";
+      }
+      if (i === (New_Calender.selected_date[2] - 1) && New_Calender.current_date[1] === New_Calender.selected_date[1] && New_Calender.current_date[0] === New_Calender.selected_date[0]) {
+      text_days += " selected";
+    }
+    text_days += "' role='day'>" + (i + 1) + "</li>";
+  }
+  document.getElementById('days').innerHTML = text_days;
+}
 
 
 /**
